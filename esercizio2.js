a8a963194b2aabfe8efdd3c795265f311cbaddc1
Shikanto <antonello.anzini.comunicazioni@gmail.com>
2021-10-15 17:26:33 +0200

    mod. ex. to add different random numbers

--- esercizio2.js
+++ esercizio2.js
@@ -1,29 +1,35 @@
-/* const array1 = [15, 2, 7];
-const array2 = [5]; */
+const array1 = [15, 2, 7];
+const array2 = [5];
 
-/* Creo elemento random da aggiugnere */
-let numberRandom = Math.floor(Math.random() * 100);
+/* const array1 = ['ciao'];
+const array2 = ['hola', 'hello']; */
 
-/* definisco i due array */
-const array1 = ['ciao'];
-const array2 = ['hola', 'hello'];
 
-/* Controllo se la lunghezza del primo è minore del secondo */
+/* Controllo le lunghezze degli array */
 
-while (array1.length < array2.length) {
-    array1.push(numberRandom);
-} 
+while (array1.length != array2.length) {
 
-/* Controllo se la lunghezza del secondo è minore del secondo */
+/* se il primo array è più piccolo aggiungo elementi  */
+   
+    if (array1.length < array2.length) {
+        let numberRandom = Math.floor(Math.random() * 100);
+        array1.push(numberRandom);
 
-while (array2.length < array1.length) {
-    array2.push(numberRandom)
-} 
+/* se il secondo array è più piccolo aggiungo elementi */    
+        
+    } else if (array2.length < array1.length) {
+        let numberRandom = Math.floor(Math.random() * 100);
+        array2.push(numberRandom);
+    } 
+    
+}
 
-/* Quando sono uguali mando un alert e stampo i due array */
+/* controllo se la lunghezza degli array è uguale e se tutto è andato a buon fine mando alert e stampo gli array*/
 
 if (array1.length === array2.length) {
+    
     alert( "i due array hanno lo stesso numero di elementi");
-    console.log(array1);
     console.log(array2);
+    console.log(array1);
+
 }
